68204a51800017b16437cc3d4ff16a504ba5e094
Sefa Bulak <zckhtln@icloud.com>
2024-12-28 01:26:34 +0300

    added styles.ts file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import { View, Text, FlatList, TouchableOpacity, StyleSheet, Dimensions, Refresh
 import { ThemedView } from '@/components/ThemedView';
 import { fetchNews, categoriesData } from '@/services/apiWp';
 import type { NewsItemWp, LoadingState, NewsDataState } from '@/types';
+import styles from '@/styles/styles';
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -168,47 +169,3 @@ export default function HomeScreen() {
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-    paddingTop: 50,
-  },
-  categoryButton: {
-    paddingHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  categoryText: {
-    fontSize: 16,
-    color: '#555',
-  },
-  selectedCategoryText: {
-    fontWeight: 'bold',
-    color: '#000',
-  },
-  underline: {
-    height: 2,
-    backgroundColor: '#000',
-    width: '100%',
-    marginTop: 5,
-  },
-  newsList: {
-    backgroundColor: 'red',
-  },
-  newsItem: {
-    width: screenWidth,
-    padding: 20,
-  },
-  newsText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#fff',
-  },
-  loader: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
---
+++ styles/styles.ts
@@ -0,0 +1,49 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    paddingTop: 50,
+  },
+  categoryButton: {
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  categoryText: {
+    fontSize: 16,
+    color: '#555',
+  },
+  selectedCategoryText: {
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  underline: {
+    height: 2,
+    backgroundColor: '#000',
+    width: '100%',
+    marginTop: 5,
+  },
+  newsList: {
+    backgroundColor: 'red',
+  },
+  newsItem: {
+    width: screenWidth,
+    padding: 20,
+  },
+  newsText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default styles;
