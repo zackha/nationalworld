13d0d0685311cc0bf15365844634007fcdc0cacf
Sefa Bulak <zckhtln@icloud.com>
2024-12-28 05:38:09 +0300

    struckture files reoptimized

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from '@/hooks/ui/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
